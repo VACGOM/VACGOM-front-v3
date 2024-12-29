56035007e6184c3974e2dcf180f8e06d53d1435e
minjeoong <orange_mj@naver.com>
2024-12-30 01:03:39 +0900

    feat : backheader 컴포넌트

--- src/component/molecule/BackHeader/style.ts
+++ src/component/molecule/BackHeader/style.ts
@@ -4,13 +4,15 @@ import { Colors, fontGenerator } from '@/styles';
 import { theme } from '@/styles/theme';
 
 export const HeaderContainer = styled.header`
-  display: flex;
-  box-sizing: border-box;
-    width: 39.3rem;
-    height: 5.4rem;
-    padding: 1rem 2rem;
+  display: grid;
+    grid-template-columns: auto 1fr 24px;
     align-items: center;
-    flex-shrink: 0;
+    justify-content: center;
+
+    width: 39.3rem;
+  height: 5.4rem;
+  padding: 1rem 2rem;
+
 
   & > .counter {
     ${fontGenerator('16px', '400', '26px')}
@@ -19,12 +21,14 @@ export const HeaderContainer = styled.header`
   }
     & > svg {
         width: 2.4rem;
+        justify-self: start;
     }
 `;
 
 export const Title = styled.h1`
-  width: 100%;
-  margin-right: 24px;
+  //width: 100%;
+  //margin-right: 24px;
+    height: fit-content;
   text-align: center;
   color: #333d4b;
   ${theme.fonts.title2}
