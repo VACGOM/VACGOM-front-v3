82978c92a29c4d990d09a2c505c6a3f6fdfb5996
minjeoong <orange_mj@naver.com>
2024-12-30 13:13:47 +0900

    feat : 버전 없는 파일 올림

---
+++ src/assets/svg/IcoCheckGray.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgIcoCheckGray = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g clipPath="url(#clip0_4695_7702)">
+      <rect width={24} height={24} rx={12} fill="white" />
+      <path
+        d="M7.91663 11.9997L10.8333 14.9163L16.6666 9.08301"
+        stroke="#B0B8C1"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_4695_7702">
+        <rect width={24} height={24} rx={12} fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default SvgIcoCheckGray;
